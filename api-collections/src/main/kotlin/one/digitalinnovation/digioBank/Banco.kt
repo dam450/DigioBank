1ed3bd8d7bd332b216663e58a2f8252781bf9537
dam450 <68165752+dam450@users.noreply.github.com>
2021-08-27 18:33:51 -0300

    update to create Banco Class

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/digioBank/Banco.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.digioBank
+
+data class Banco(
+        val nome:String,
+        val numero:Int
+) {
+fun info() = "$nome - $numero"
+}
---
+++ api-collections/src/main/kotlin/one/digitalinnovation/digioBank/testes/BancoTeste.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.digioBank.testes
+
+import one.digitalinnovation.digioBank.Banco
+
+fun main() {
+    val digioBank = Banco( nome = "DigiOne", numero = 12)
+
+    println(digioBank.nome)
+    println(digioBank.numero)
+    println(digioBank.info())
+
+}
