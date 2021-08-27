e70b9eaff9a57537363e0727a9eb49fd8c843acc
dam450 <68165752+dam450@users.noreply.github.com>
2021-08-27 17:04:46 -0300

    created person class

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/digioBank/Pessoa.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digioBank
+
+class Pessoa {
+    var nome: String = "Jether"
+    var cpf: String = "123.123.123-12"
+}
+
+fun main() {
+    val jether = Pessoa()
+
+    println(jether.nome)
+    println(jether.cpf)
+}
