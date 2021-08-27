4b8d040682f8e6d9be06dbbeaaa570d2dcbcd778
dam450 <68165752+dam450@users.noreply.github.com>
2021-08-27 17:48:51 -0300

    propiedade cpf setada como private set

--- api-collections/src/main/kotlin/one/digitalinnovation/digioBank/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/digioBank/Pessoa.kt
@@ -1,13 +1,17 @@
 package one.digitalinnovation.digioBank
 
 class Pessoa {
-    var nome: String = "Jether"
-    var cpf: String = "123.123.123-12"
+    var nome: String = "Jether" //variavel publica
+    var cpf: String = "123.456.789-10" //variavel privada
+    private set
 }
 
 fun main() {
     val jether = Pessoa()
 
+    println(jether)
+    //jether.cpf = "4321"
     println(jether.nome)
     println(jether.cpf)
+
 }
