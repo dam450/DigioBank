674a7a32ceae93ab4e2eebc33594174bf676c230
dam450 <68165752+dam450@users.noreply.github.com>
2021-08-27 17:56:05 -0300

    update to set class members

--- api-collections/src/main/kotlin/one/digitalinnovation/digioBank/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/digioBank/Pessoa.kt
@@ -4,14 +4,16 @@ class Pessoa {
     var nome: String = "Jether" //variavel publica
     var cpf: String = "123.456.789-10" //variavel privada
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome, $cpf"
 }
 
 fun main() {
     val jether = Pessoa()
+    println(jether.pessoaInfo())
 
-    println(jether)
-    //jether.cpf = "4321"
-    println(jether.nome)
-    println(jether.cpf)
+
 
 }
