db8fb4d220adaef602e975ea1f6999b75cbdfeb8
dam450 <68165752+dam450@users.noreply.github.com>
2021-08-27 18:43:39 -0300

    update to info format

--- api-collections/src/main/kotlin/one/digitalinnovation/digioBank/testes/BancoTeste.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/digioBank/testes/BancoTeste.kt
@@ -5,8 +5,8 @@ import one.digitalinnovation.digioBank.Banco
 fun main() {
     val digioBank = Banco( nome = "DigiOne", numero = 12)
 
-    println(digioBank.nome)
-    println(digioBank.numero)
+    //println(digioBank.nome)
+    //println(digioBank.numero)
     println(digioBank.info())
 
 }
